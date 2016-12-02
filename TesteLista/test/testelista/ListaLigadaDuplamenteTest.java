d057b3954cfbdf6c8e2b41e32cb8ac7ed3e5961f
amandagobus <amandagobus@gmail.com>
2016-12-02 11:09:08 -0200

    teste

--- TesteLista/test/testelista/ListaLigadaDuplamenteTest.java
+++ TesteLista/test/testelista/ListaLigadaDuplamenteTest.java
@@ -17,39 +17,79 @@ import static org.junit.Assert.*;
  * @author Amanda Gobus
  */
 public class ListaLigadaDuplamenteTest {
-    
+
     public ListaLigadaDuplamenteTest() {
     }
 
-
-    /**
-     * Test of getLast method, of class ListaLigadaDuplamente.
-     */
-    @Test
-    public void testGetLast() {
-        System.out.println("getLast");
-        ListaLigadaDuplamente instance = new ListaLigadaDuplamente();
-        Object expResult = null;
-        Object result = instance.getLast();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-
+//    /**
+//     * Test of getLast method, of class ListaLigadaDuplamente.
+//     */
+//    @Test
+//    public void testGetLast() {
+//        System.out.println("getLast");
+//        ListaLigadaDuplamente instance = new ListaLigadaDuplamente();
+//        Object expResult = null;
+//        Object result = instance.getLast();
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
     /**
      * Test of add method, of class ListaLigadaDuplamente.
      */
     @Test
     public void testAdd_GenericType() {
         System.out.println("add");
-        String e ="a";
-        String a = "1";
+        String e = "a";
+       // String a = "1";
         List<String> instance = new ListaLigadaDuplamente();
-        boolean expResult =true;
+        boolean expResult = true;
         boolean result = instance.add(e);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
+        
+        System.out.println("Amanda");
+         e = "amanda";
+       //  a = "amandah";
+        expResult = true;
+        result = instance.add(e);
+        assertEquals(expResult, result);
+
+        System.out.println("hahasdfasdflkjhglkjgpoiuertoiumnzxcnvbbvcnxzmzcnzxczx");
+         e = "hahasdfasdflkjhglkjgpoiuertoiumnzxcnvbbvcnxzmzcnzxczx";
+       //  a = "amandah";
+        expResult = true;
+        result = instance.add(e);
+        assertEquals(expResult, result);
+        
+         System.out.println("123456789");
+         e = "123456789";
+       //  a = "amandah";
+        expResult = true;
+        result = instance.add(e);
+        assertEquals(expResult, result);
+        
+         System.out.println("1a2b3c4d5e");
+         e = "1a2b3c4d5e";
+       //  a = "amandah";
+        expResult = true;
+        result = instance.add(e);
+        assertEquals(expResult, result);
+        
+         System.out.println(" ");
+         e = " ";
+         //  a = "amandah";
+        expResult = false;
+        result = instance.add(e);
+        assertEquals(expResult, result);
+        
+        
+        
+
+
+        
+        
+
         
     }
 
@@ -64,11 +104,16 @@ public class ListaLigadaDuplamenteTest {
         boolean expResult = false;
         boolean result = instance.remove(o);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
+        
+         System.out.println("remove");
+         o = "BomDiaAmanda";
+         expResult = true;
+         result = instance.remove(o);
+         assertEquals(expResult, result);
+
     }
 
-    
     
     
 }
